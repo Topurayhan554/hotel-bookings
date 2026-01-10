95e0260acdfd23ea3f71b0620f7c77ef9cf867cf
Topurayhan554 <topurayhantipu@gmail.com>
2026-01-10 11:33:27 +0600

    all done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div
       className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white 
-    bg-[url("/src/assets/heroImage.png")] bg-no-repeat bg-cover bg-center h-screen'
+    bg-[url("/src/assets/hotel-bookings.jpg")] bg-no-repeat bg-cover bg-center h-screen'
     >
       <p className="bg-[#49b9ff]/50 px-3.5 py-1 rounded-full mt-20 hover:scale-105 transition-transform duration-300">
         Your Journey Begins Here
